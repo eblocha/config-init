db23a7f4a6f131c933c1bb56b76494780c7a1723
eblocha <eblocha@protonmail.com>
2022-06-18 07:27:46 -0400

    add tests for the yaml initializer

---
+++ tests/test_yaml.py
@@ -0,0 +1,128 @@
+from pathlib import Path
+from textwrap import dedent
+import unittest
+from click.testing import CliRunner
+
+from config_init.initializers import YamlInitializer
+
+
+def get_decl(path: Path):
+    return f"# yaml-language-server: $schema={path}"
+
+
+class TestYamlDirect(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.runner = CliRunner()
+
+    def setUp(self) -> None:
+        self.default = dedent(
+            """
+        name: Test
+        email: test@example.com
+        """
+        )
+        self.schema = {"$schema": ""}
+
+    def default_with_schema(self, path: Path):
+        return dedent(
+            f"""{get_decl(path)}
+            {self.default}"""
+        )
+
+    def get_config(self, path: Path):
+        self.assertTrue(path.exists())
+
+        with path.open() as f:
+            return f.read()
+
+    def test_noschema(self):
+        initializer = YamlInitializer(self.default)
+
+        path = Path("test.yml")
+
+        with self.runner.isolated_filesystem():
+            initializer.init(path)
+            written = self.get_config(path)
+            self.assertEqual(written, self.default)
+
+    def test_direct_schema(self):
+        initializer = YamlInitializer(self.default, schema=self.schema)
+
+        path = Path("test.yml")
+
+        with self.runner.isolated_filesystem():
+            schema_path = Path("schema") / "schema.json"
+            schema_path.parent.mkdir(exist_ok=True)
+            initializer.init(path, schema_path=schema_path)
+            written = self.get_config(path)
+
+            self.assertEqual(written, self.default_with_schema(schema_path))
+
+    def test_direct_schema_nopath(self):
+        """Make sure we remove a schema reference when no schema path is provided"""
+        schema_path = Path("schema") / "schema.json"
+
+        withschema = self.default_with_schema(schema_path)
+
+        initializer = YamlInitializer(withschema, schema=self.schema)
+
+        path = Path("test.yml")
+
+        with self.runner.isolated_filesystem():
+
+            schema_path.parent.mkdir(exist_ok=True)
+            initializer.init(path)
+            written = self.get_config(path)
+
+            self.assertEqual(written, self.default)
+
+    def test_direct_schema_off(self):
+        """Make sure we remove a schema reference when forced off"""
+        schema_path = Path("schema") / "schema.json"
+
+        initializer = YamlInitializer(
+            self.default_with_schema(schema_path), schema=self.schema
+        )
+
+        path = Path("test.yml")
+
+        with self.runner.isolated_filesystem():
+
+            schema_path.parent.mkdir(exist_ok=True)
+            initializer.init(path, schema_path=schema_path, inject_schema=False)
+            written = self.get_config(path)
+
+            self.assertEqual(written, self.default)
+
+    def test_schema_otherdir(self):
+        """Test the case when the schema is in another directory"""
+        schema_path = Path("schema") / "schema.json"
+        initializer = YamlInitializer(self.default, schema=self.schema)
+
+        path = Path("config/test.yml")
+
+        with self.runner.isolated_filesystem():
+            schema_path.parent.mkdir(exist_ok=True)
+            path.parent.mkdir(exist_ok=True)
+
+            initializer.init(path, schema_path=schema_path)
+            written = self.get_config(path)
+
+            self.assertEqual(written, self.default_with_schema("../schema/schema.json"))
+
+    def test_schema_existing(self):
+        """Test the case when the default already has a schema declaration"""
+        initializer = YamlInitializer(
+            self.default_with_schema("some-schema.json"), schema=self.schema
+        )
+
+        path = Path("test.yml")
+
+        with self.runner.isolated_filesystem():
+            schema_path = Path("schema") / "schema.json"
+            schema_path.parent.mkdir(exist_ok=True)
+            initializer.init(path, schema_path=schema_path)
+            written = self.get_config(path)
+
+            self.assertEqual(written, self.default_with_schema(schema_path))
